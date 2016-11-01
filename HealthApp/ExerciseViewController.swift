9eb2780d96b5ee41b73250cc159f263faff5df69
Wong Sam <wongsam@Wongde-MacBook-Pro.local>
2016-11-01 23:06:59 +0800

    Hi

---
+++ HealthApp/ExerciseViewController.swift
@@ -0,0 +1,170 @@
+//
+//  ExerciseViewController.swift
+//  HealthApp
+//
+//  Created by Wong Sam on 27/10/2016.
+//  Copyright © 2016年 Wong Sam. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+import Charts
+
+class ExerciseViewController: UIViewController,ChartViewDelegate {
+    var myHealthStore = HKHealthStore()
+    @IBOutlet var stepCountLabel : UILabel!
+    @IBOutlet weak var stepBarChartView: BarChartView!
+    var steps: [Int] = [0,0,0,0,0,0,0]
+    var days: [Int] = [1,2,3,4,5,6,7]
+    let goal : Int = Int(7000)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stepBarChartView.delegate = self
+        stepBarChartView.chartDescription?.text = ""
+        stepBarChartView.leftAxis.axisMinimum = Double(0)
+        stepBarChartView.rightAxis.axisMinimum = Double(0)
+        stepBarChartView.leftAxis.axisMaximum = Double(20000)
+        stepBarChartView.rightAxis.axisMaximum = Double(20000)
+        stepBarChartView.scaleXEnabled = false
+        stepBarChartView.xAxis.avoidFirstLastClippingEnabled = true
+        //stepBarChartView.noDataText = "Loading data for the chart."
+        setChart(values: steps)
+        readWeeklyStep()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func readMonthlyStep(){
+        steps = []
+        days = []
+        let calendar = NSCalendar.current
+        let compoents = calendar.dateComponents([.day], from: NSDate() as Date)
+        if compoents.day!>30{
+            for day in 0...30{
+                self.steps.insert(0, at: day)
+                self.days.insert(day+1, at: day)
+            }
+        }
+        else{
+            for day in 0..<compoents.day!{
+                self.steps.insert(0, at: day)
+                self.days.insert(day+1, at: day)
+            }
+            for day in compoents.day!...29{
+                self.steps.insert(0, at: day)
+                self.days.insert(day+1, at: day)
+            }
+        }
+        for day in 0..<compoents.day!{
+            let typeOfStep = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
+            let scCalendar = Calendar.init(identifier: Calendar.Identifier.gregorian)
+            let now = NSDate()
+            let startDate = scCalendar.date(byAdding: Calendar.Component.day, value: day-compoents.day!+1, to: now as Date)
+            let today = scCalendar.startOfDay(for: startDate! as Date)
+            let yesterday = scCalendar.date(byAdding: Calendar.Component.day, value: -1, to: today)
+            let predicate = HKQuery.predicateForSamples(withStart: today, end: NSDate() as Date , options: .strictStartDate)
+            let interval: NSDateComponents = NSDateComponents()
+            interval.day = 1
+            //  Perform the Query
+            let query = HKStatisticsCollectionQuery(quantityType: typeOfStep!, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: today as Date, intervalComponents:interval as DateComponents)
+            
+            query.initialResultsHandler = { query, results, error in
+                //  Something went Wrong
+                if error != nil {
+                    return
+                }
+                if let myResults = results{
+                    myResults.enumerateStatistics(from: yesterday! as Date, to: today as Date) {
+                        statistics, stop in
+                        if let quantity = statistics.sumQuantity() {
+                            let step = (Int(quantity.doubleValue(for: HKUnit.count())))
+                            self.steps[day] = step
+                            self.setChart(values: self.steps)
+                            self.stepCountLabel.text = "\(self.steps[compoents.day!-1])"
+                        }
+                    }
+                }
+            }
+            myHealthStore.execute(query)
+        }
+    }
+    private func readWeeklyStep(){
+        steps = []
+        days = []
+        let calendar = NSCalendar.current
+        let weekDay = calendar.component(.weekday, from: NSDate() as Date)
+        for day in 0..<weekDay{
+            let currentday = calendar.date(byAdding: Calendar.Component.day, value: day-weekDay+1, to: NSDate() as Date)
+            let compoent = calendar.dateComponents([.day], from: currentday!)
+            self.days.insert(compoent.day!, at: day)
+            self.steps.insert(0, at: day)
+        }
+        
+        for day in 0..<weekDay{
+            let typeOfStep = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
+            let scCalendar = Calendar.init(identifier: Calendar.Identifier.gregorian)
+            let now = NSDate()
+            let startDate = scCalendar.date(byAdding: Calendar.Component.day, value: day-weekDay+1, to: now as Date)
+            let today = scCalendar.startOfDay(for: startDate! as Date)
+            let yesterday = scCalendar.date(byAdding: Calendar.Component.day, value: -1, to: today)
+            let predicate = HKQuery.predicateForSamples(withStart: today, end: NSDate() as Date , options: .strictStartDate)
+            let interval: NSDateComponents = NSDateComponents()
+            interval.day = 1
+            //  Perform the Query
+            let query = HKStatisticsCollectionQuery(quantityType: typeOfStep!, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: today as Date, intervalComponents:interval as DateComponents)
+            
+            query.initialResultsHandler = { query, results, error in
+                //  Something went Wrong
+                if error != nil {
+                    return
+                }
+                if let myResults = results{
+                    myResults.enumerateStatistics(from: yesterday! as Date, to: today as Date) {
+                        statistics, stop in
+                        if let quantity = statistics.sumQuantity() {
+                            let step = (Int(quantity.doubleValue(for: HKUnit.count())))
+                            self.steps[day] = step
+                            self.setChart(values: self.steps)
+                            self.stepCountLabel.text = "\(self.steps[weekDay-1])"
+                        }
+                    }
+                }
+            }
+            myHealthStore.execute(query)
+        }
+    }
+    func setChart(values: [Int]){
+        var dataEntries: [BarChartDataEntry] = []
+        let ll = ChartLimitLine(limit: Double(self.goal), label: "")
+        stepBarChartView.rightAxis.addLimitLine(ll)
+        stepBarChartView.leftAxis.addLimitLine(ll)
+        for i in 0..<values.count {
+            let dataEntry = BarChartDataEntry(x: Double(days[i]), y: Double(values[i]))
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        stepBarChartView.data = chartData
+        stepBarChartView.xAxis.labelPosition = .bottom
+
+    }
+    
+    @IBAction func weeklyButton() {
+        readWeeklyStep()
+    }
+    
+    @IBAction func monthlyButton() {
+        readMonthlyStep()
+    }
+    
+    @IBAction func profileButton() {
+        if let ViewController = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+            present(ViewController, animated: false, completion:nil)
+        }
+    }
+}
--- HealthApp/ViewController.swift
+++ HealthApp/ViewController.swift
@@ -7,19 +7,146 @@
 //
 
 import UIKit
+import HealthKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    var myHealthStore = HKHealthStore()
+    @IBOutlet var ageLabel : UILabel!
+    @IBOutlet var bloodLabel : UILabel!
+    @IBOutlet var sexLabel : UILabel!
+    @IBOutlet var heightLabel : UILabel!
+    @IBOutlet var weightLabel : UILabel!
+    @IBOutlet var BMILabel : UILabel!
+    @IBOutlet var stepCountLabel : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        requestAuthorization()
     }
-
-
+    
+    private func requestAuthorization(){
+        let typeOfRead = Set(arrayLiteral:
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!,
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!,
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!,
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)!,
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.biologicalSex)!,
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!
+        )
+        myHealthStore.requestAuthorization(toShare: nil, read: typeOfRead, completion: { (success, error) in
+            if let e = error {
+                print("Error: \(e.localizedDescription)")
+                return
+            }
+            if success {
+                print("Success!")
+                self.readData()
+            } else {
+                print("Error!")
+            }
+        })
+    }
+    
+    //Data Reading
+    private func readData() {
+        do {
+            // get sex
+            let myBiologicalSex = try myHealthStore.biologicalSex()
+            var myBiologicalSexText:String = ""
+            switch myBiologicalSex.biologicalSex {
+            case HKBiologicalSex.female:
+                myBiologicalSexText = "Female"
+            case HKBiologicalSex.male:
+                myBiologicalSexText = "Male"
+            case HKBiologicalSex.notSet:
+                myBiologicalSexText = "NotSet"
+            default:
+                myBiologicalSexText = "error"
+                print("error")
+            }
+            // assign sex to label
+            sexLabel.text = "Sex : \(myBiologicalSexText)"
+            // get blood type
+            let myBloodType = try myHealthStore.bloodType()
+            var myBloodTypeText = ""
+            switch myBloodType.bloodType {
+            case HKBloodType.aPositive:
+                myBloodTypeText = "A+"
+            case HKBloodType.aNegative:
+                myBloodTypeText = "A-"
+            case HKBloodType.bPositive:
+                myBloodTypeText = "B+"
+            case HKBloodType.bNegative:
+                myBloodTypeText = "B-"
+            case HKBloodType.abPositive:
+                myBloodTypeText = "AB+"
+            case HKBloodType.abNegative:
+                myBloodTypeText = "AB-"
+            case HKBloodType.oPositive:
+                myBloodTypeText = "O+"
+            case HKBloodType.oNegative:
+                myBloodTypeText = "O-"
+            default:
+                myBloodTypeText = "error"
+                print("error")
+            }
+            // assign blood type to label
+            bloodLabel.text = "BloodType : \(myBloodTypeText)"
+            // get date of birth
+            let dateOfBirth = try myHealthStore.dateOfBirthComponents()
+            let now = NSDate()
+            let calendar = NSCalendar.current
+            let compoents = calendar.dateComponents([.year], from: now as Date)
+            // calculate age and assign to label
+            ageLabel.text = "Age : \(compoents.year! - dateOfBirth.year!)"
+            // get step count
+            let typeOfStep = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
+            let scCalendar = Calendar.init(identifier: Calendar.Identifier.gregorian)
+            let startDate = scCalendar.startOfDay(for: now as Date)
+            let yesterday = scCalendar.date(byAdding: Calendar.Component.day, value: -1, to: startDate)
+            let predicate = HKQuery.predicateForSamples(withStart: startDate, end: NSDate() as Date, options: .strictStartDate)
+            let interval: NSDateComponents = NSDateComponents()
+            interval.day = 1
+            //  Perform the Query
+            let query = HKStatisticsCollectionQuery(quantityType: typeOfStep!, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: startDate as Date, intervalComponents:interval as DateComponents)
+            
+            query.initialResultsHandler = { query, results, error in
+                //  Something went Wrong
+                if error != nil {                    
+                    return
+                }
+                if let myResults = results{
+                    myResults.enumerateStatistics(from: yesterday! as Date, to: NSDate() as Date) {
+                        statistics, stop in
+                        if let quantity = statistics.sumQuantity() {
+                            let steps: Int = Int(quantity.doubleValue(for: HKUnit.count()))
+                            self.stepCountLabel.text = "Steps : \(steps)"
+                        }
+                    }
+                }
+            }
+            
+            myHealthStore.execute(query)
+          
+            
+        } catch let error as NSError {
+            // reading error
+            sexLabel.text = "Sex : Error"
+            bloodLabel.text = "BloodType : Error"
+            ageLabel.text = "Age: Error"
+            stepCountLabel.text = "StepCount: Error"
+            print("A read error occured")
+            print("\(error.localizedDescription)")
+        }
+    }
+    @IBAction func exerciseButton() {
+        if let exerciseViewController = storyboard?.instantiateViewController(withIdentifier: "ExerciseViewController") as? ExerciseViewController {
+            present(exerciseViewController, animated: false, completion:nil)
+        }
+    }
 }
-
